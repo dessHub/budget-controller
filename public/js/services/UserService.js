8a6a37999baabd7ba2e4e0d0dd8ea81b137bb675
Zachariah Ngonyani <zachariahn@gmail.com>
2016-09-14 18:22:31 +0300

    Format userService

--- public/js/services/UserService.js
+++ public/js/services/UserService.js
@@ -1,27 +1,23 @@
 angular.module('UserService', []).factory('Budgets', ['$http', function($http) {
 
+    return {
+      // call to get all budgets
 
-return {
-          // call to get all budgets
+      get : function() {
+        return $http.get('/');
+      },
 
-get : function() {
-              return $http.get('/');
-          },
+      // call to POST and create a new budget
 
+      create : function(data) {
+        return $http.post('/add', data);
+      },
 
-
-            // call to POST and create a new budget
-
-create : function(data) {
-              return $http.post('/add', data);
-          },
-
-          // call to DELETE a budget
-          delete : function(id) {
-              return $http.delete('/budget/' + id);
-          
-          }
+      // call to DELETE a budget
+      delete : function(id) {
+        return $http.delete('/budget/' + id);
 
       }
 
+    }
 }]);
